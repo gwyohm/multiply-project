3d665ca3a36bc34ac7d060f2b11f2d4dd674ff52
Guillaume LEPICARD <guillaume.lepicard@wedia.fr>
2020-02-15 11:48:49 +0100

    citer micmaths

--- screens/GraphicTableScreen.js
+++ screens/GraphicTableScreen.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect} from 'react';
-import { View, Text } from 'react-native';
+import {View, Text, Linking} from 'react-native';
 import { Svg, Path } from 'react-native-svg';
+import LinkButton  from '../components/LinkButton';
+import styles from '../styles'
+
 export default function GraphicTableScreen({ }) {
   const [table, setTable] = useState(0);
   const speed = 50;
@@ -25,13 +28,30 @@ export default function GraphicTableScreen({ }) {
   useEffect(() => () => clearTimeout(cancelTimeout));
 
   return (
-    <View style={{flex: 1, justifyContent: 'space-between', alignItems: 'center'}}>
+    <View style={[styles.container, {flex: 1, justifyContent: 'space-between', alignItems: 'center'}]}>
       <View style={{flex: 1, paddingTop: 10}}>
         <Svg viewBox="-1 -1 2 2" width={200} height={200}>
           <Path transform="rotate(-90)" strokeWidth="0.01" stroke="rgba(0,0,0,0.2)" d={res}/>
         </Svg>
         <Text style={{paddingTop: 20}}>table des { table.toFixed(2) } modulo {modulo}</Text>
       </View>
+      <View style={{paddingTop: 20}}>
+        <Text>
+          Pour plus d'informations sur cette animation, rendez-vous sur la chaîne
+          <LinkButton
+            onPress={() => Linking.openURL('https://www.youtube.com/user/Micmaths')}
+            text="Micmaths"
+          />
+          de Mickaël Launay...
+          <LinkButton
+            onPress={() => Linking.openURL('https://www.youtube.com/watch?v=-X49VQgi86E')}
+            text="La face cachée des tables de multiplication"
+          />
+        </Text>
+      </View>
     </View>
   );
+}
+GraphicTableScreen.navigationOptions = {
+  title: 'Bonus'
 }
